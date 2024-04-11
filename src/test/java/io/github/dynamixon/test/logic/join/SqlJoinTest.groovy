145cd9b52b6cb1b20919c37e6685dda71577ae67
maojianfeng <jianfeng.mao2@caocaoglobal.com>
2024-04-11 16:50:30 +0800

    1.4.0 release

--- src/test/java/io/github/dynamixon/test/logic/join/SqlJoinTest.groovy
+++ src/test/java/io/github/dynamixon/test/logic/join/SqlJoinTest.groovy
@@ -5,6 +5,7 @@ import io.github.dynamixon.flexorm.misc.InterceptorContext
 import io.github.dynamixon.flexorm.pojo.Cond
 import io.github.dynamixon.flexorm.pojo.Join
 import io.github.dynamixon.flexorm.pojo.LeftJoin
+import io.github.dynamixon.flexorm.pojo.OrderCond
 import io.github.dynamixon.flexorm.pojo.RightJoin
 import io.github.dynamixon.moredata.JoinTableA
 import io.github.dynamixon.test.logic.LogicTestBase
@@ -78,7 +79,7 @@ class SqlJoinTest implements LogicTestBase{
             ).findObjects(JoinTableA, new Cond('id',123L),new Cond('TBL_B.int_f',456),new Cond('TBL_C.varchar_f','abc'))
         }
         validator = genValidator(
-            'select * from join_table_A TBL_A  left join join_table_B TBL_B on TBL_A.id  = TBL_B.id  right join join_table_C TBL_C on TBL_A.id  = TBL_C.id  where TBL_A.id = ? and TBL_B.int_f = ? and TBL_C.varchar_f = ?',
+            'select * from join_table_A TBL_A  left join join_table_B TBL_B on TBL_A.id  = TBL_B.id  right join join_table_C TBL_C on TBL_A.id  = TBL_C.id  where TBL_A.id = ? and TBL_B.int_f = ? and TBL_C.varchar_f = ? order by TBL_A.id desc',
             [123L,456,'abc']
         )
 
@@ -86,6 +87,7 @@ class SqlJoinTest implements LogicTestBase{
             it.prep(
                 sqlId(LogicTester.sqlId4Logic(it,'join-basic-all-col')),
                 intercept(LogicTester.getDelegatedInterceptor([new JoinTableA()],validator,[(DIALECT_KEY):it.getDialectType()])),
+                order(new OrderCond('id','desc')),
                 joinTable('TBL_A',[
                     new LeftJoin('join_table_B','TBL_B','id','id'),
                     new RightJoin('join_table_C','TBL_C','id','id')
