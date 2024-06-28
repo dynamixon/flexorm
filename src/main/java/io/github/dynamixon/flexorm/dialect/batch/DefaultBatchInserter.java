8e4f36e2a848e886d73bc4be842d8f24f54006a9
mao <halflife3@163.com>
2024-06-28 23:18:39 +0800

    DefaultBatchInserter employs jdbc batch

--- src/main/java/io/github/dynamixon/flexorm/dialect/batch/DefaultBatchInserter.java
+++ src/main/java/io/github/dynamixon/flexorm/dialect/batch/DefaultBatchInserter.java
@@ -5,7 +5,10 @@ import io.github.dynamixon.flexorm.enums.SqlExecutionInterceptorChainMode;
 import io.github.dynamixon.flexorm.misc.ExtraParamInjector;
 import io.github.dynamixon.flexorm.misc.SqlExecutionInterceptor;
 import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -14,17 +17,30 @@ public class DefaultBatchInserter implements BatchInserter {
     public int batchInsert(CoreRunner coreRunner, String table, List<Map<String, Object>> listMap){
         int affected = 0;
         if(CollectionUtils.isNotEmpty(listMap)){
-            String sqlId = ExtraParamInjector.getSqlId();
-            SqlExecutionInterceptor sqlExecutionInterceptor = ExtraParamInjector.getSqlInterceptor();
-            SqlExecutionInterceptorChainMode sqlInterceptorChainMode = ExtraParamInjector.getSqlInterceptorChainMode();
-            boolean interceptorSpan = sqlExecutionInterceptor !=null&& sqlExecutionInterceptor.spanWithin();
-            for (Map<String, Object> map : listMap) {
-                ExtraParamInjector.sqlId(sqlId);
-                if(interceptorSpan){
-                    ExtraParamInjector.interceptWithChainMode(sqlExecutionInterceptor,sqlInterceptorChainMode!=null?sqlInterceptorChainMode:SqlExecutionInterceptorChainMode.CHAIN_AFTER_GLOBAL);
-                }
-                affected += coreRunner.insert(table,map);
+            List<String> columns = new ArrayList<>(listMap.get(0).keySet());
+            StringBuilder sql = new StringBuilder("insert into " + table + " (");
+            StringBuilder valueSql = new StringBuilder(" values (");
+            List<List<Object>> valuesArr = new ArrayList<>();
+            for (String column : columns) {
+                sql.append(column).append(",");
+                valueSql.append("?,");
             }
+            sql = new StringBuilder(StringUtils.stripEnd(sql.toString(), ",") + ") ");
+            valueSql = new StringBuilder(StringUtils.stripEnd(valueSql.toString(), ",") + ")");
+            sql.append(valueSql);
+            for (Map<String, Object> map : listMap) {
+                List<Object> values = new ArrayList<>();
+                for (String column : columns) {
+                    Object value = map.get(column);
+                    values.add(value);
+                }
+                valuesArr.add(values);
+            }
+            Object[][] array = valuesArr.stream()
+                    .map(innerList -> innerList.toArray(new Object[0]))
+                    .toArray(Object[][]::new);
+            int[] nums = coreRunner.genericBatchUpdate(sql.toString(), array);
+            affected = Arrays.stream(nums).sum();
         }
         return affected;
     }
--- src/main/java/io/github/dynamixon/flexorm/dialect/batch/StandardBatchInserter.java
+++ src/main/java/io/github/dynamixon/flexorm/dialect/batch/StandardBatchInserter.java
@@ -17,20 +17,20 @@ public class StandardBatchInserter {
     public static int batchInsert(CoreRunner coreRunner, String table, List<Map<String, Object>> listMap) {
         int affectedNum = 0;
         if(CollectionUtils.isNotEmpty(listMap)){
-            List<String> fields = new ArrayList<>(listMap.get(0).keySet());
-            int fieldNum = fields.size();
+            List<String> columns = new ArrayList<>(listMap.get(0).keySet());
+            int colNum = columns.size();
             StringBuilder sql = new StringBuilder("insert into " + table + " (");
             StringBuilder valueSql = new StringBuilder(" values");
             List<Object> values = new ArrayList<>();
-            for (String field : fields) {
-                sql.append(field).append(",");
+            for (String column : columns) {
+                sql.append(column).append(",");
             }
             sql = new StringBuilder(StringUtils.stripEnd(sql.toString(), ",") + ") ");
             for (Map<String, Object> map : listMap) {
                 valueSql.append(" ( ");
-                for(int i=0;i<fieldNum;i++){
-                    String field = fields.get(i);
-                    Object value = map.get(field);
+                for(int i=0;i<colNum;i++){
+                    String column = columns.get(i);
+                    Object value = map.get(column);
                     valueSql.append("?,");
                     values.add(value);
                 }
